26a08becd746e71a2f13474994d3f1fd0e9893ca
xavier.banos <xavier.banos@edreamsodigeo.com>
2022-01-11 11:50:24 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # create-wrapper-components-kfp
+
+Usage
+```py
+from create_wrapper_components import create_wrapper_components
+from your_package import py_func_component_1, py_func_component_2
+components = [py_func_component_1, py_func_component_2]
+create_wrapper_components(components)
+```
+
+py_func_component should have input & output args typed according to kgp rules in your package
---
+++ create_wrapper_components.py
@@ -0,0 +1,70 @@
+"""Create wrapper components."""
+import inspect
+
+_SCRIPT_START = """\"\"\"Components.\"\"\"
+from typing import NamedTuple
+
+from edo.ml_plumber import Constants
+from kfp.v2.dsl import component
+
+base_image = Constants().DEFAULT_IMAGE
+
+
+# get_output_path = create_component_from_func_in_base_image(
+#     func=get_output_path,
+#     base_image=base_image
+# )
+"""
+
+
+def get_func_source(func):
+    """Get func source."""
+    name = func.__name__
+    signature = inspect.signature(func)
+    return_annotation = signature.return_annotation
+    signature = str(signature)
+    signature = signature.split('->')
+    if len(signature) != 2:
+        raise ValueError
+
+    params = signature[0]
+    return_types = signature[1]
+
+    params = params.replace(')', '\n    )')
+
+    if '**' in params:
+        params = params[:params.index('**')]
+        params = params[:params.rfind(',')]
+        params = f"{params})"
+
+    if hasattr(return_annotation, '_field_types'):
+        return_types = return_annotation._field_types
+        return_types = [f"('{k}', {v.__name__})" for k, v in return_types.items()]
+        return_types = ',\n'.join(return_types)
+        return_types = f" NamedTuple('{return_annotation.__name__}',[{return_types}]): # noqa"
+
+    signature = f"{params}->{return_types}"
+
+    func_source = f'def {name}{signature}:\n' \
+                  f'    """Kfp wrapper."""\n' \
+                  '    kwargs = locals()\n' \
+                  '    kwargs = {k: None if v == "None" else v for k, v in kwargs.items()}\n' \
+                  f'    from {func.__module__} import {name}\n' \
+                  f'    return {name}(**kwargs)\n'
+    return func_source
+
+
+def create_wrapper_components(components, install_kfp_package=True,
+                              filename='tmp/components.py'):
+    """Create wrapper components."""
+    out = [_SCRIPT_START]
+    for component in components:
+        aux = f'@component(base_image=base_image, install_kfp_package={install_kfp_package})\n'
+        aux += get_func_source(component)
+        out += [aux]
+
+    out = '\n\n'.join(out)
+    # print(out)
+    with open(filename, 'w') as f:
+        f.write(out)
+    return
